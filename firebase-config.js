479af68a4f4827bfd2581327845809bd7a51c759
Paul Snr-Dev <paulstackweb@gmail.com>
2026-06-18 19:58:09 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey:            "AIzaSyDv-suSTi9VA0z0uENsRG_9YVzYT4D5ocY",
-  authDomain:        "forzafood-82d93.firebaseapp.com",
-  projectId:         "forzafood-82d93",
-  storageBucket:     "forzafood-82d93.firebasestorage.app",
+  apiKey: "AIzaSyDv-suSTi9VA0z0uENsRG_9YVzYT4D5ocY",
+  authDomain: "forzafood-82d93.firebaseapp.com",
+  projectId: "forzafood-82d93",
+  storageBucket: "forzafood-82d93.firebasestorage.app",
   messagingSenderId: "439515361367",
-  appId:             "1:439515361367:web:bdc5e9c9cf4949e9ad3312"
+  appId: "1:439515361367:web:bdc5e9c9cf4949e9ad3312",
+  measurementId: "G-LG8EKD2NY6"
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
